6ebcedcb8e8db2435205d3d44afec93eb967876d
GilbertII <gilbert.abuan.ii@gmail.com>
2022-05-07 02:46:58 +0800

    add counter button event

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,9 +1,15 @@
 import classes from "./Counter.module.css";
 
 function Counter() {
+  let count = 0;
+  function onClickHandler() {
+    count++;
+    console.log(count);
+  }
+
   return (
     <div>
-      <button>+1</button>
+      <button onClick={onClickHandler}>+1</button>
       <span className={classes.count}>0</span>
     </div>
   );
