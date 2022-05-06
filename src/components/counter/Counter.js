17e87089126a938defa5677b7513d85e5ec26da0
GilbertII <gilbert.abuan.ii@gmail.com>
2022-05-07 03:06:19 +0800

    add state counter

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,18 +1,49 @@
+import { Component, useState } from "react";
 import classes from "./Counter.module.css";
 
+class Counter extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0,
+    };
+
+    // bind function
+    this.increamentHandler = this.increamentHandler.bind(this);
+  }
+
+  increamentHandler() {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.increamentHandler}>+1</button>
+        <span className={classes.count}>{this.state.counter}</span>
+      </div>
+    );
+  }
+}
+
+/*
+// Function Format
 function Counter() {
-  let count = 0;
+  const [count, setCount] = useState(0);
+
   function onClickHandler() {
-    count++;
-    console.log(count);
+    setCount(count + 1);
   }
 
   return (
     <div>
       <button onClick={onClickHandler}>+1</button>
-      <span className={classes.count}>0</span>
+      <span className={classes.count}>{count}</span>
     </div>
   );
 }
-
+*/
 export default Counter;
