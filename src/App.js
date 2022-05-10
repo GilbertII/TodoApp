a5badb9c80921df45a2655042504de801aac5d46
GilbertII <gilbert.abuan.ii@gmail.com>
2022-05-11 01:16:45 +0800

    controlled component - adding state to login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Counter from "./components/counter/Counter";
 
 import "./App.css";
 import TodoApp from "./components/todo/TodoApp";
--- src/components/todo/LoginComponent.js
+++ src/components/todo/LoginComponent.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
 
 class LoginComponent extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      password: "",
+    };
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
     return (
       <div>
-        Username: <input type="text" name="username" />
-        Password: <input type="password" name="password" />
+        Username:{" "}
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleOnChange}
+        />
+        Password:{" "}
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleOnChange}
+        />
         <button>Login</button>
       </div>
     );
